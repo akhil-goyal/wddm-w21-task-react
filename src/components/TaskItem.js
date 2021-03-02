480dbfb0dc5dce0c47fcbb950463a022c3b7bc2f
Rocco P <roccopan@gmail.com>
2021-03-02 15:34:29 -0500

    Added our first state variable

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'css/TaskItem.css'
 
 const TaskItem = ({ id, task, complete, onUpdateTask }) => {
 
-  console.log(`TaskItem: constructor`)
+  console.log(`TaskItem()`)
+  
+  // Local variable (built from a prop)
+  // let c = complete
 
+  // State variable
+  let [c, setComplete] = useState(complete)
+
+
+
+  
   const handleChange = (event) => {
     console.log(`check`, event.currentTarget.checked)
-    complete = !complete
-    onUpdateTask(id, complete)
+    
+    // c = !c
+    setComplete(!c)
+    
+    //onUpdateTask(id, c)
 
     //this.complete = eleInput.checked
     //this.dispatchEvent(customCompleteEvent)
   }
 
   return (<li>
-    <input type="checkbox" id={`task-${id}`} name={`task-${id}`} checked={complete} onChange={handleChange} />
+    <input type="checkbox" id={`task-${id}`} name={`task-${id}`} checked={c} onChange={handleChange} />
     <label for={`task-${id}`} className="task">{task}</label>
   </li>)
 }
